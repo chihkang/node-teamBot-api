caec6796c6dff28acf7aabc992c5903a68c850d6
ChihKang <chihkang@me.com>
2017-06-26 23:35:19 +0800

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,26 @@
+// const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
+
+// var obj = new ObjectID();
+// console.log(obj);
+
+MongoClient.connect('mongodb://localhost:27017/teamWorkApp', (err, db) => {
+  if(err){
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  db.collection('CertDetails').findOneAndUpdate({
+    _id: new ObjectID('595111c8d32254aba8f64aea')
+  }, {
+    $set: {
+      HintCode: 'CA9990'
+    }
+  },{
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
+
+  // db.close();
+});
