e62db12f6487ceb5e9cdbebab0d83a0ae9d0cfc5
ChihKang <chihkang@me.com>
2017-06-27 21:48:12 +0800

    Add POST /CertDetails route and refactor mongoose

---
+++ server/server.js
@@ -0,0 +1,26 @@
+var { mongoose } = require('./db/mongoose')
+var { CertDetails } = require('./models/CertDetails');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/CertDetails', (req, res) => {
+  var certThings = new CertDetails({
+    "HintCode": req.body.HintCode,
+    "Description": req.body.Description
+  });
+  // console.log(req.body);
+  certThings.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Started on port 3000.');
+});
